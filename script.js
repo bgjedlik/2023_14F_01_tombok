00027452d56bcfce72812c12fdbc65690e1cabb3
bgjedlik <bolya.gabor@jedlik.eu>
2023-09-22 08:37:08 +0200

    add style

--- script.js
+++ script.js
@@ -3,11 +3,23 @@
 let nev = document.querySelector('#nev');
 let saveButton = document.querySelector('.save-button');
 let result = document.querySelector('.result');
+let error = document.querySelector('.error');
+
 // console.log(nev);
 // console.log(saveButton);
 let emberek = [];
 
 function hozzaadNev(){
+
+    // ellenőrzés
+    if (nev.value == ''){
+        error.style.display = 'inline-block';
+        return;
+    } else {
+        error.style.display = 'none';
+    }
+
+
     result.innerHTML = ''; // törli a HTML tartalmat
     //console.log('OK');
     //console.log(nev.value);
@@ -16,7 +28,7 @@ function hozzaadNev(){
     emberek.push(nev.value);
     //console.log(emberek);
 
-    let ul = document.createElement('ul');
+    let ul = document.createElement('ul'); // <ul></ul>
     //console.log(ul);
     emberek.forEach(ember => {
         let li = document.createElement('li');  //<li></li>
@@ -25,6 +37,9 @@ function hozzaadNev(){
     } );
 
     result.append(ul);
+
+    nev.value = ''; // név mező törlése
+    nev.focus(); // ráviszi a fókuszt az elemre, aktív lesz 
 }
 
 saveButton.addEventListener('click',hozzaadNev);
